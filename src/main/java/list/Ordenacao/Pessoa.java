3169d3f7661e0f54666bc16e91e8be252d950a0e
Sergio M B <110953280+Mefest0@users.noreply.github.com>
2024-07-10 13:35:15 -0300

    Resolução atividade CatalogoLivros e Ordenação pessoa

--- src/main/java/list/Ordenacao/Pessoa.java
+++ src/main/java/list/Ordenacao/Pessoa.java
@@ -2,48 +2,50 @@ package main.java.list.Ordenacao;
 
 import java.util.Comparator;
 
-public class Pessoa implements Comparable<Pessoa> {
-  //atributos
-  private String nome;
-  private int idade;
-  private double altura;
+public class Pessoa implements Comparable <Pessoa> {
+    private String nome;
 
-  public Pessoa(String nome, int idade, double altura) {
-    this.nome = nome;
-    this.idade = idade;
-    this.altura = altura;
-  }
+    private int idade;
 
-  @Override
-  public int compareTo(Pessoa p) {
-    return Integer.compare(idade, p.getIdade());
-  }
+    private double altura;
 
-  public String getNome() {
-    return nome;
-  }
+    public Pessoa(String nome, double altura, int idade) {
+        this.nome = nome;
+        this.altura = altura;
+        this.idade = idade;
+    }
 
-  public int getIdade() {
-    return idade;
-  }
+    @Override
+    public int compareTo(Pessoa p) {
+        return Integer.compare(idade, p.getIdade());
+    }
 
-  public double getAltura() {
-    return altura;
-  }
+    public String getNome() {
+        return nome;
+    }
 
-  @Override
-  public String toString() {
-    return "Pessoa{" +
-        "nome='" + nome + '\'' +
-        ", idade=" + idade +
-        ", altura=" + altura +
-        '}';
-  }
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", altura=" + altura +
+                '}';
+    }
 }
 
 class ComparatorPorAltura implements Comparator<Pessoa> {
-  @Override
-  public int compare(Pessoa p1, Pessoa p2) {
-    return Double.compare(p1.getAltura(), p2.getAltura());
-  }
+
+    @Override
+    public int compare(Pessoa p1, Pessoa p2) {
+        return Double.compare(p1.getAltura(), p2.getAltura());
+    }
 }
